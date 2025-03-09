4ca813448d72b1ebc66c84d849d94c8b804b1c32
hatef eidi <hatef.121.h@gmail.com>
2025-03-09 22:30:26 +0000

    Add solution - CodeHub

---
+++ 8-kyu/calculate-bmi/calculate-bmi.js
@@ -0,0 +1,15 @@
+function bmi(weight, height) {
+ const BMI=weight/Math.pow(height, 2)
+ if(BMI<=18.5){
+   return "Underweight"
+ }
+  else if(BMI<=25.0){
+    return "Normal"
+  }
+  else if(BMI<=30.0){
+    return "Overweight"
+  }
+  else{
+    return "Obese"
+  }
+}
